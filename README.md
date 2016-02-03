37d0e8565c676d78518c00dee69cdc6394f51dc5
Alexei Drake <alexei.drake@nuclei.co>
2016-02-03 23:28:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ember-qrcode
+
+An Ember addon that draws QR codes. Provides a `{{qr-code}}` component that produces a `canvas.qr-code` element.
---
+++ app/components/qr-code.js
@@ -0,0 +1,55 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend(
+{
+	tagName: 'canvas',
+	classNames: ['qr-code'],
+	attributeBindings: ['width', 'height'],
+	width: 200,
+	height: 200,
+	light: '#FFFFFF',
+	dark: '#000000',
+	ctx: null,
+	data: '',
+
+	didInsertElement: function()
+	{
+		this.set('ctx', this.get('element').getContext('2d'));
+		return this.draw();
+	},
+
+	empty: function()
+	{
+		var ctx = this.get('ctx');
+		ctx.fillStyle = this.get('light');
+		return ctx.fillRect(0, 0, this.get('width'), this.get('height'));
+	},
+
+	draw: function()
+	{
+		this.empty();
+		var data = this.get('data');
+		var ctx = this.get('ctx');
+		var width = this.get('width');
+		var height = this.get('height');
+		
+		var qr = new QRCode(0, 1);
+		qr.addData(data);
+		qr.make();
+
+		var size = qr.getModuleCount();
+
+		var cwidth = Math.round(width / size);
+		var cheight = Math.round(height / size);
+		var padding = 0;
+		
+		function cx(x) {return x * cwidth;};
+		function cy(y) {return y * cheight;};
+
+		ctx.fillStyle = this.get('dark');
+		for (var row = 0; row < size; ++row)
+			for (var col = 0; col < size; ++col)
+				if (qr.isDark(row, col))
+					ctx.fillRect(cx(row) + padding, cy(col) + padding, cwidth - padding, cheight - padding);
+	}
+});
